c647cf982f89eabcb08f1cdf89a48dd956a90c17
sfgardev <sf.garol@gmail.com>
2024-12-10 15:05:01 +0700

    feat: button component

---
+++ src/shared/ui/button/button.tsx
@@ -0,0 +1,21 @@
+import { ComponentPropsWithoutRef, forwardRef } from 'react'
+import { cn } from '../../lib'
+
+type Props = {} & ComponentPropsWithoutRef<'button'>
+
+export const Button = forwardRef<HTMLButtonElement, Props>(({ className, children, ...props }, ref) => {
+  return (
+    <button
+      ref={ref}
+      className={cn(
+        'rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none',
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  )
+})
+
+Button.displayName = 'Button'
