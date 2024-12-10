6bc7932754d525ca5150275f8a58b06026173ab3
sfgardev <sf.garol@gmail.com>
2024-12-10 17:00:07 +0700

    style: change button styles

--- src/shared/ui/button/button.tsx
+++ src/shared/ui/button/button.tsx
@@ -8,7 +8,7 @@ export const Button = forwardRef<HTMLButtonElement, Props>(({ className, childre
     <button
       ref={ref}
       className={cn(
-        'rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none',
+        'bg-primary text-primary-foreground shadow hover:bg-primary/90 h-9 px-4 py-2 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
         className
       )}
       {...props}
